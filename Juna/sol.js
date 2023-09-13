5e45d4ff40e589b6af2454ac407554d30d67dac7
Junatius <cjuniar0@gmail.com>
2023-09-14 00:19:53 +0800

    fix: change the function used

--- Juna/sol.js
+++ Juna/sol.js
@@ -3,45 +3,32 @@ function asynchronousRequest(args) {
     return new Promise((resolve, reject) => {
         // Checking if the argument is filled
         if (!args) {
-            reject("You Have to Set Something")
+            reject("Whoa! Something went wrong.")
         } else {
             setTimeout(function() {
-                resolve(
-                    args + ' ' + Math.floor(Math.random() * 10)   
-                )
+                resolve({ 
+                    body: args + ' ' + Math.floor(Math.random() * 10)}   
+                );
             }, 500);
         }
     })
 }
 
-// Make the Promise to using the function and print it
-Promise.allSettled([ asynchronousRequest('First'), asynchronousRequest('Second'), asynchronousRequest('Third')])
-.then((result) => console.log(result));
-/* 
-Expected Output will be [
-  { status: 'fulfilled', value: 'First 1' },
-  { status: 'fulfilled', value: 'Second 8' },
-  { status: 'fulfilled', value: 'Third 5' }
-]
-*/
-
-/* 
-We can also make async function like this below one if the result will depends each other
-Since the result only build by using the function and take random argument we made,
-So we're gonna using the Promise.allSettled()
-
+// Make the function
 async function asyncTesting() {
     try {
         const first = await asynchronousRequest('First');
         const second = await asynchronousRequest('Second');
         const third = await asynchronousRequest('Third');
 
-        console.log(first, second, third);
+        console.log(first.body, second.body, third.body);
     } catch(e) {
         console.error(e);
     }
 }
 
-Execute the function
+// Execute the function
 asyncTesting();
-*/
+
+// Expected Output Will be : 
+// First 4 Second 3 Third 5
