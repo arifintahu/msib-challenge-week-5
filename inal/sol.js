187e208565fb437872fd8af415623b5f83fd6293
Inal Mahpud <inalmahpud1@gmail.com>
2023-09-16 14:47:17 +0700

    mengganti nama variable

--- inal/sol.js
+++ inal/sol.js
@@ -1,29 +1,25 @@
 function asynchronousRequest(args) {
-    return new Promise((resolve, reject) => {
-      if (!args) {
-        reject(new Error("Whoa! Something went wrong."));
-      } else {
-          setTimeout(() =>{
-            // Just adding in a random number so it seems like the contrived asynchronous function
-            // returned different data
-            resolve({body: args + " " + Math.floor(Math.random() * 10)})
-          },500)
-      }
-    });
-  }
+  return new Promise((resolve, reject) => {
+    if (!args) {
+      reject(new Error("Whoa! Something went wrong."));
+    } else {
+      setTimeout(() => {
+        resolve({ body: args + " " + Math.floor(Math.random() * 10) });
+      }, 500);
+    }
+  });
+}
 
-// Nested asynchronous requests
 async function callbackHell() {
   try {
-    const satu = await asynchronousRequest("first");
-    console.log(satu.body)
-    const dua = await asynchronousRequest("second");
-    console.log(dua.body)
-    const tiga = await asynchronousRequest(null);
-    console.log(tiga.body)
+    const one = await asynchronousRequest("first");
+    console.log(one.body);
+    const two = await asynchronousRequest("second");
+    console.log(two.body);
+    const three = await asynchronousRequest(null);
+    console.log(three.body);
   } catch (e) {
     console.error(e);
   }
 }
 callbackHell();
-
