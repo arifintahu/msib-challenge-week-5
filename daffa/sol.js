7e012e1899ba1d69d4e8893448f347e988cd2e39
daffasatrianegara <daffasatria036@gmail.com>
2023-09-18 10:09:16 +0700

    promise

---
+++ daffa/sol.js
@@ -0,0 +1,75 @@
+function asynchronousRequest(args) {
+  return new Promise((resolve, reject) => {
+    if (!args) {
+      reject("ERROR");
+    } else {
+      setTimeout(function () {
+        resolve(null, { body: args + " " + Math.floor(Math.random() * 10) });
+      }, 500);
+    }
+  });
+}
+
+// Nested asynchronous requests
+function callbackHell() {
+  asynchronousRequest("First").then((response) => {
+      console.log(response.body);
+      return asynchronousRequest("Second");
+    })
+    .then((response) => {
+      console.log(response.body);
+      return asynchronousRequest(null);
+    })
+    .then((response) => {
+      console.log(response.body);
+    })
+    .catch((error) => {
+        console.log('Error', error); 
+    })
+}
+
+// Execute
+callbackHell();
+
+
+// function asynchronousRequest(args, callback) {
+//     // Throw an error if no arguments are passed
+//     if (!args) {
+//       return callback(new Error('Whoa! Something went wrong.'))
+//     } else {
+//       return setTimeout(
+//         // Just adding in a random number so it seems like the contrived asynchronous function
+//         // returned different data
+//         () => callback(null, { body: args + ' ' + Math.floor(Math.random() * 10) }),
+//         500,
+//       )
+//     }
+//   }
+
+//   // Nested asynchronous requests
+//   function callbackHell() {
+//     asynchronousRequest('First', function first(error, response) {
+//       if (error) {
+//         console.log(error)
+//         return
+//       }
+//       console.log(response.body)
+//       asynchronousRequest('Second', function second(error, response) {
+//         if (error) {
+//           console.log(error)
+//           return
+//         }
+//         console.log(response.body)
+//         asynchronousRequest(null, function third(error, response) {
+//           if (error) {
+//             console.log(error)
+//             return
+//           }
+//           console.log(response.body)
+//         })
+//       })
+//     })
+//   }
+
+//   // Execute
+//   callbackHell()
